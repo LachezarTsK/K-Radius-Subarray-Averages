160f3ac5089420b97ce620490c5fbf2753293139
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-06-27 20:02:36 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,25 +3,25 @@ public class Solution {
 
     private static final int NOT_POSSIBLE_TO_CREATE_TARGET_SUBARRAY = -1;
 
-    public int[] getAverages(int[] nums, int radius) {
+    public int[] getAverages(int[] inputNumbers, int radius) {
         if (radius == 0) {
-            return nums;
+            return inputNumbers;
         }
 
         int head = 0;
         int tail = 0;
         long sumTargetSubarray = 0;
-        int[] subarraysAverages = new int[nums.length];
+        int[] subarraysAverages = new int[inputNumbers.length];
 
-        while (head < nums.length) {
+        while (head < inputNumbers.length) {
 
-            sumTargetSubarray += nums[head];
-            if (head - radius < 0 || head + radius > nums.length - 1) {
+            sumTargetSubarray += inputNumbers[head];
+            if (head - radius < 0 || head + radius > inputNumbers.length - 1) {
                 subarraysAverages[head] = NOT_POSSIBLE_TO_CREATE_TARGET_SUBARRAY;
             }
             if ((head - tail) == 2 * radius) {
                 subarraysAverages[tail + radius] = (int) (sumTargetSubarray / (2 * radius + 1));
-                sumTargetSubarray -= nums[tail];
+                sumTargetSubarray -= inputNumbers[tail];
                 ++tail;
             }
             ++head;
