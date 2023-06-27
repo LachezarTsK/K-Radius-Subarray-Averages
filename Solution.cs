b5fecfac68c7a1f8198bf44fa29e1f159c7f347f
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-06-27 20:10:09 +0200

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -1,31 +1,33 @@
 
-#include <vector>
-using namespace std;
+using System;
+using System.Collections.Generic;
 
-class Solution {
-    
-    inline static const int NOT_POSSIBLE_TO_CREATE_TARGET_SUBARRAY = -1;
-
-public:
-
-    vector<int> getAverages(const vector<int>& inputNumbers, int radius) const {
-        if (radius == 0) {
+public class Solution
+{
+    const int NOT_POSSIBLE_TO_CREATE_TARGET_SUBARRAY = -1;
+    public int[] GetAverages(int[] inputNumbers, int radius)
+    {
+        if (radius == 0)
+        {
             return inputNumbers;
         }
 
         int head = 0;
         int tail = 0;
         long sumTargetSubarray = 0;
-        vector<int> subarraysAverages(inputNumbers.size());
+        int[] subarraysAverages = new int[inputNumbers.Length];
 
-        while (head < inputNumbers.size()) {
+        while (head < inputNumbers.Length)
+        {
 
             sumTargetSubarray += inputNumbers[head];
-            if (head - radius < 0 || head + radius > inputNumbers.size() - 1) {
+            if (head - radius < 0 || head + radius > inputNumbers.Length - 1)
+            {
                 subarraysAverages[head] = NOT_POSSIBLE_TO_CREATE_TARGET_SUBARRAY;
             }
-            if ((head - tail) == 2 * radius) {
-                subarraysAverages[tail + radius] = (sumTargetSubarray / (2 * radius + 1));
+            if ((head - tail) == 2 * radius)
+            {
+                subarraysAverages[tail + radius] = (int)(sumTargetSubarray / (2 * radius + 1));
                 sumTargetSubarray -= inputNumbers[tail];
                 ++tail;
             }
@@ -33,4 +35,4 @@ public:
         }
         return subarraysAverages;
     }
-};
+}
